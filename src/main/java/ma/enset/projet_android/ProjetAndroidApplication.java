c93871e921f22c93a4d2ed022a453751a50a0fe0
bertrand <bertrandkafando07@gmail.com>
2022-04-08 12:26:11 +0000

    first commit

---
+++ src/main/java/ma/enset/projet_android/ProjetAndroidApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.projet_android;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetAndroidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetAndroidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/projet_android/ProjetAndroidApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.projet_android;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetAndroidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
