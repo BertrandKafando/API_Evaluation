5f9ac1c9d4f0f2ca34e5d8ec5ae4c507f4853778
bertrand <bertrandkafando07@gmail.com>
2022-04-08 13:17:02 +0000

    entité

--- src/main/java/ma/enset/projet_android/entities/Questionnaire.java
+++ src/main/java/ma/enset/projet_android/entities/Questionnaire.java
@@ -4,14 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+@Entity
 @Data @AllArgsConstructor @NoArgsConstructor
 public class Questionnaire {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long questionnaireId;
     private String title;
     private String description;
+    @OneToMany(mappedBy = "questionnaire",fetch = FetchType.EAGER)
+    List<Question>questions=new ArrayList<>();
+    @ManyToOne()
+    private Professeur professeur;
+    @ManyToMany()
+    List<Etudiant>etudiants=new ArrayList<>();
 }
