537ed207773ac0ce3045971bf4aefa2711c99e8e
bertrand <bertrandkafando07@gmail.com>
2022-04-08 14:51:48 +0000

    controller

---
+++ src/main/java/ma/enset/projet_android/web/PostController.java
@@ -0,0 +1,24 @@
+package ma.enset.projet_android.web;
+
+import lombok.AllArgsConstructor;
+import ma.enset.projet_android.entities.Etudiant;
+import ma.enset.projet_android.repositories.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+public class PostController {
+    EtudiantRepository etudiantRepository;
+    ProfesseurRepository professeurRepository;
+    QuestionnaireRepository questionnaireRepository;
+    QuestionRepository questionRepository;
+    ReponseRepository reponseRepository;
+
+    @PostMapping(path = "/etudiant")
+    void add(Model model, @RequestBody Etudiant etudiant){
+        if(etudiant!=null){
+            etudiantRepository.save(etudiant);
+        }
+    }
+}
